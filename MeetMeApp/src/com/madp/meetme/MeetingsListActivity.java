c5751a48431881c7e7195b6fff15cd9b71bd8d69
esauali <saulius.alisauskas@ericsson.com>
2012-02-25 18:29:05 +0100

    Add retrieve user account details and enter user email

--- MeetMeApp/src/com/madp/meetme/MeetingsListActivity.java
+++ MeetMeApp/src/com/madp/meetme/MeetingsListActivity.java
@@ -1,23 +1,33 @@
 package com.madp.meetme;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
 import com.madp.meetme.common.entities.Meeting;
 import com.madp.meetme.webapi.WebService;
 import com.madp.utils.Logger;
 import com.madp.utils.MeetingsListAdapter;
 import com.madp.utils.SerializerHelper;
+import com.madp.utils.Statics;
 
 //TODO: add logs for debugging
 //TODO: refreshList() should be done on a separate thread and should display progress
@@ -31,30 +41,54 @@ import com.madp.utils.SerializerHelper;
  * 
  */
 public class MeetingsListActivity extends ListActivity {	
-	private final String TAG = "MeetingListActivity";
-	private List<Meeting> meetings  = new ArrayList();;
+	private final String TAG = "MeetingListActivity";	
+	private List<Meeting> meetings;
 	private MeetingsListAdapter listAdapter;
 	private ImageButton newMeetingButton, exitButton;
 	private static final int CREATE_NEW_MEETING_RESULT = 891030;
 	private static final int CLICK_ON_MEETING_LIST_ELEMENT_RESULT = 860604;
-	private WebService ws;
+	private WebService ws;	
+	private Context mContext;
+	private AlertDialog userNameDialog = null;
 	
-	
-	
-	
-	
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.meetinglist);
-
-		
+		mContext = this;
 		ws = new WebService(new Logger());
 		
 		newMeetingButton = (ImageButton) findViewById(R.id.new_meeting_button);
 		exitButton = (ImageButton) findViewById(R.id.logout);
 
+		/*
+		 * We should have user name and user email in the properties.		 * 
+		 */
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());		
+		if (prefs.getString(Statics.USERNAME, null) == null){
+			showUserNameDialog(); // user will enter his name here	
+		} else {
+			Log.d(TAG, "User name:"+prefs.getString(Statics.USERNAME, null));
+		}
+		
+		if (prefs.getString(Statics.USEREMAIL, null) == null){
+			// get default account email
+			final SharedPreferences.Editor prefsEditor = prefs.edit();	
+			Account[] accounts = AccountManager.get(this).getAccountsByType("com.google");
+			if (accounts.length > 1) {		
+				Account account = accounts[0];
+				Log.d(TAG, "Found account type:"+account.type+" name:"+account.name+"saving...");		
+				prefsEditor.putString(Statics.USEREMAIL, account.name);
+				prefsEditor.commit();
+			} else {
+				Toast.makeText(this, "Could not get users email, using noemail@default.com", Toast.LENGTH_SHORT).show();
+				Log.e(TAG, "Could not get users email, using noemail@default.com");
+				prefsEditor.putString(Statics.USEREMAIL, "noemail@default.com");
+				prefsEditor.commit();
+			}
+		} else {
+			Log.d(TAG, "User email:"+prefs.getString(Statics.USEREMAIL, null));	
+		}		
 		
 		if(meetings == null)
 			refreshList();
@@ -117,9 +151,8 @@ public class MeetingsListActivity extends ListActivity {
 	/**
 	 * Fetch meetings from server
 	 */
-	
-	
 	private void refreshList() {
+		Log.d(TAG, "refreshList");
 		List<Meeting> meetingsTmp = ws.getMeetings(0, 100);
 		if (meetingsTmp != null) {
 			meetings = meetingsTmp;
@@ -129,5 +162,35 @@ public class MeetingsListActivity extends ListActivity {
 			}
 		}
 		
+	}
+	
+	private void showUserNameDialog(){
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());		
+		if (prefs.getString(Statics.USERNAME, null) == null){
+			final SharedPreferences.Editor prefsEditor = prefs.edit();		
+				
+			//Create dialog where user will enter his name			
+            LayoutInflater factory = LayoutInflater.from(this);
+            final View textEntryView = factory.inflate(R.layout.dialog_user_name_entry, null);            
+            AlertDialog userNameDialog = new AlertDialog.Builder(this)                
+                .setTitle("Please enter your name")
+                .setView(textEntryView)
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {         
+                    	EditText editText = (EditText) textEntryView.findViewById(R.id.name);
+                    	String name = editText.getText().toString();
+                    	if (name.length() > 3){
+                    		prefsEditor.putString(Statics.USERNAME, name);
+                    		prefsEditor.commit();
+                    	} else {
+                    		Toast.makeText(mContext, "Name should be > 3 characters", Toast.LENGTH_SHORT).show();
+                    		showUserNameDialog();
+                    	}
+                    }
+                })  
+                .setCancelable(false)               
+                .create();
+           userNameDialog.show();
+		}
 	}
 }
---
+++ MeetMeApp/src/com/madp/utils/Statics.java
@@ -0,0 +1,6 @@
+package com.madp.utils;
+
+public class Statics {
+	public static final String USERNAME = "userName";
+	public static final String USEREMAIL = "userEmail";
+}
