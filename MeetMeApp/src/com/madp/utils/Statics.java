1732aa6bd669bd010efabd62d55d5f33c785dd7d
esauali <saulius.alisauskas@ericsson.com>
2012-02-25 19:25:13 +0100

    Add author and comment in Statics.java

--- MeetMeApp/src/com/madp/utils/Statics.java
+++ MeetMeApp/src/com/madp/utils/Statics.java
@@ -1,5 +1,10 @@
 package com.madp.utils;
 
+/**
+ * Class for storing default string throughout application
+ * @author esauali 2012-02-25
+ *
+ */
 public class Statics {
 	public static final String USERNAME = "userName";
 	public static final String USEREMAIL = "userEmail";
