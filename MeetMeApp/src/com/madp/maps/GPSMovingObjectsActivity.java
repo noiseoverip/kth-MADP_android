e461bb06fb8e12111f2e5a8d9d146919eb9ae603
Anargyros Chatzaras <anacha@kth.se>
2012-02-27 15:00:31 +0100

    testing the map

--- MeetMeApp/src/com/madp/maps/GPSMovingObjectsActivity.java
+++ MeetMeApp/src/com/madp/maps/GPSMovingObjectsActivity.java
@@ -6,6 +6,8 @@ import com.madp.meetme.R;
 import com.madp.meetme.common.entities.LatLonPoint;
 import com.madp.meetme.common.entities.Meeting;
 import com.madp.meetme.common.entities.User;
+import com.madp.meetme.webapi.WebService;
+import com.madp.utils.Logger;
 import com.madp.utils.SerializerHelper;
 import android.location.Location;
 import android.location.LocationManager;
@@ -17,9 +19,9 @@ import android.widget.Toast;
 
 public class GPSMovingObjectsActivity extends GPSActivity {
 
-	
-	User[] user_group =null;
-	
+
+	WebService ws;
+	Meeting t = null;
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
@@ -32,35 +34,28 @@ public class GPSMovingObjectsActivity extends GPSActivity {
 			e.printStackTrace();
 		}
 	}
-	
-	public void MovePersons(){
-		for(int i=0;i<user_group.length;i++){
-			/*
-			user_group[i].MoveTo(user_group[i].getCoordinates().getDLatitude()+0.1,
-					user_group[i].getCoordinates().getDLongitude()+0.1);
-			Log.i(LOCATION_SERVICE,"Moved "+user_group[i]+"at "+user_group[i].getCurrentPosition().getILatitude()+" - "+user_group[i].getCurrentPosition().getILongitude());
-			*/
-		}
+
+	public void PollServer(){
+		Runnable runnable = new Runnable(){
+			public void run(){       		
+                  Log.i("WEB_SERVICE","will try to fetch meeting "+t.getId());
+                  t = (Meeting) ws.getMeeting(t.getId());
+			}			
+		};
+		new Thread(runnable).start();
 	}
 	
 	public void MoveQuietly(){		
 		Runnable runnable = new Runnable(){
 			public void run(){       		
-        		for(int i=0;i<50;i++){
-	                Log.i(LOCATION_SERVICE,"will try to draw them");
-	                MovePersons();
-	                try {
-						Thread.sleep(1000);
-						for(int j=0;j<user_group.length;j++){
-								 DrawAtMap(user_group[j].getCoordinates(),user_group[j].getName(),"Android pos",R.drawable.androidmarker );
-						//		 Mymap.postInvalidate();						 
-							}
-						} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+                  Log.i(LOCATION_SERVICE,"will try to draw them");
+					for(int j=0;j<t.getParticipants().size();j++){
+							 DrawAtMap(t.getParticipants().get(j).getCoordinates(),
+									   t.getParticipants().get(j).getName(),
+									   t.getAddress(),R.drawable.androidmarker );
+					//		 Mymap.postInvalidate();						 
 					}
                 }			
-			}
 		};
 		new Thread(runnable).start();
 	}
@@ -69,7 +64,8 @@ public class GPSMovingObjectsActivity extends GPSActivity {
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		MoveQuietly();
+		PollServer();
+		//MoveQuietly();
 	}
 
 	
@@ -79,25 +75,18 @@ public class GPSMovingObjectsActivity extends GPSActivity {
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.gps_map);
         Init();
+        ws = new WebService(new Logger()); 
         
         Bundle b= getIntent().getExtras();
-		Meeting t = (Meeting) SerializerHelper.deserializeObject(b.getByteArray("meeting"));
+		t = (Meeting) SerializerHelper.deserializeObject(b.getByteArray("meeting"));
 		if(t == null){
 			Log.e("Meeting", "De-serilialisation error");
 		}
-		
-        //Show my self on the map
-        if(locman.isProviderEnabled(LocationManager.GPS_PROVIDER)){ 
-            System.out.println("will find coordinates by GPS");
-            locman.requestLocationUpdates(LocationManager.GPS_PROVIDER,2,2, loclis);
-            Location location=locman.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            DrawAtMap(new LatLonPoint(location.getLatitude(), location.getLongitude()),"Argyris","mypos",R.drawable.androidmarker );          
-        }
-        else{
-        	System.out.println("GPS unavailable");
-            Toast.makeText(this,"GPS is disabled.", Toast.LENGTH_LONG).show();             
-        }
         DrawAtMap(t.getCoordinates(),t.getTitle(),"Meeting location",R.drawable.bluedot );       
+		for(int i=0;i<t.getParticipants().size();i++){
+			DrawAtMap(t.getParticipants().get(i).getCoordinates(),
+					t.getParticipants().get(i).getName(),t.getTitle(), R.drawable.androidmarker);
+		}
         mapcon.animateTo(new GeoPoint(t.getCoordinates().getILatitude(), t.getCoordinates().getILongitude()));
         mapcon.setZoom(10);
         
