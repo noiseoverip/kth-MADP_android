ad5249c69f42bf54f22296e5186a1b83ec6c5e46
esauali <saulius.alisauskas@ericsson.com>
2012-03-10 23:35:52 +0100

    Improve NewMeetingActivity
    Add map button listener to launch GPSPointOnMap activity
    Add saving meeting location from results of GPSPointOnMap activity
    Removed setting of dummy owner

--- MeetMeApp/src/com/madp/meetme/NewMeetingActivity.java
+++ MeetMeApp/src/com/madp/meetme/NewMeetingActivity.java
@@ -18,7 +18,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
@@ -63,102 +62,106 @@ import com.madp.utils.Statics;
  * 
  */
 public class NewMeetingActivity extends ListActivity {
-	
+
+	private static final int REQUEST_CODE_POINTONMAP = 100;
+
 	class LatLonPoint {
-		/*a convinient class to use when using coordinates and maps*/
+		/* a convinient class to use when using coordinates and maps */
 		private static final long serialVersionUID = -4119420152413060192L;
 		int lat;
-	    int lon;
+		int lon;
+
 		public LatLonPoint(double latitude, double longitude) {
-	        lat = (int)(latitude * 1E6);
-	        lon = (int) (longitude * 1E6);
-	       	Log.i(null, "point at lat "+latitude+" lon "+longitude);        
-	    }
-	    public int getILongitude(){
-	    	return lon;
-	    }
-	    public int getILatitude(){
-			return lat;    	
-	    }
-	    
-	    public double getDLongitude(){
-	    	return lon/1E6;
-	    }
-	    public double getDLatitude(){
-			return lat/1E6;    	
-	    }
+			lat = (int) (latitude * 1E6);
+			lon = (int) (longitude * 1E6);
+			Log.i(null, "point at lat " + latitude + " lon " + longitude);
+		}
+
+		public int getILongitude() {
+			return lon;
+		}
+
+		public int getILatitude() {
+			return lat;
+		}
+
+		public double getDLongitude() {
+			return lon / 1E6;
+		}
+
+		public double getDLatitude() {
+			return lat / 1E6;
+		}
 	}
-	
-	public LatLonPoint  getLatLong(JSONObject jsonObject) {
-		/*extract a LatLonPoint from JSON object received from google server*/
-        Double lon = new Double(0);
-        Double lat = new Double(0);
 
-        try {
+	public LatLonPoint getLatLong(JSONObject jsonObject) {
+		/* extract a LatLonPoint from JSON object received from google server */
+		Double lon = new Double(0);
+		Double lat = new Double(0);
 
-            lon = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
-                .getJSONObject("geometry").getJSONObject("location")
-                .getDouble("lng");
+		try {
 
-            lat = ((JSONArray)jsonObject.get("results")).getJSONObject(0)
-                .getJSONObject("geometry").getJSONObject("location")
-                .getDouble("lat");
+			lon = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+					.getJSONObject("location").getDouble("lng");
 
-        } catch (Exception e) {
-            e.printStackTrace();
+			lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+					.getJSONObject("location").getDouble("lat");
 
-        }
+		} catch (Exception e) {
+			e.printStackTrace();
 
-        Log.i("coordinates::","Lat = "+lat+" Lon = "+lon);
-        return new LatLonPoint(lat,lon);
-    }
-	
+		}
+
+		Log.i("coordinates::", "Lat = " + lat + " Lon = " + lon);
+		return new LatLonPoint(lat, lon);
+	}
+
 	public static JSONObject getLocationInfo(String address) {
-		/*pull from google server, longitude and latitude of a String address*/
-		Log.i("address info", "address = "+address);
-		
-	    StringBuilder stringBuilder = new StringBuilder();
-	    try {
+		/* pull from google server, longitude and latitude of a String address */
+		Log.i("address info", "address = " + address);
 
-	    address = address.replaceAll(" ","%20");    
+		StringBuilder stringBuilder = new StringBuilder();
+		try {
 
-	    HttpPost httppost = new HttpPost("http://maps.google.com/maps/api/geocode/json?address=" + address + "&sensor=false");
-	    HttpClient client = new DefaultHttpClient();
-	    HttpResponse response;
-	    stringBuilder = new StringBuilder();
-	        response = client.execute(httppost);
-	        HttpEntity entity = response.getEntity();
-	        InputStream stream = entity.getContent();
-	        int b;
-	        while ((b = stream.read()) != -1) {
-	            stringBuilder.append((char) b);
-	        }
-	    } catch (ClientProtocolException e) {
-	    } catch (IOException e) {
-	    }
+			address = address.replaceAll(" ", "%20");
 
-	    JSONObject jsonObject = new JSONObject();
-	    try {
-	        jsonObject = new JSONObject(stringBuilder.toString());
-	    } catch (JSONException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	    }
+			HttpPost httppost = new HttpPost("http://maps.google.com/maps/api/geocode/json?address=" + address
+					+ "&sensor=false");
+			HttpClient client = new DefaultHttpClient();
+			HttpResponse response;
+			stringBuilder = new StringBuilder();
+			response = client.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			InputStream stream = entity.getContent();
+			int b;
+			while ((b = stream.read()) != -1) {
+				stringBuilder.append((char) b);
+			}
+		} catch (ClientProtocolException e) {
+		} catch (IOException e) {
+		}
 
-	    return jsonObject;
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject = new JSONObject(stringBuilder.toString());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return jsonObject;
 	}
-	
-	
+
 	private static final String TAG = "NewMeetingActivity";
-	private EditText nameOfMeeting, nameOfPlace;	
-	private ImageButton createMeeting, meetingTime, addParticipants, meetingDate;
-	private TextView infolabel,currentDateSet, currentTimeSet;
+	private EditText nameOfMeeting, nameOfPlace;
+	private ImageButton createMeeting, meetingTime, addParticipants, meetingDate, map;
+	private TextView infolabel, currentDateSet, currentTimeSet;
 	private ParticipantsAdapter p_adapter;
 	private static final int TIME_DIALOG_ID = 0;
 	private static final int DATE_DIALOG_ID = 1;
 	private int hour, min, year, month, day;
 	private long timeLeftToMeetingInMillisec;
-	private static final int CONTACT_PICKER_RESULT = 12345;	
+	private static final int CONTACT_PICKER_RESULT = 12345;
 	private Meeting meeting;
 	private WebService ws;
 	private AlarmManager am;
@@ -179,6 +182,7 @@ public class NewMeetingActivity extends ListActivity {
 		infolabel = (TextView) findViewById(R.id.infolabel);
 		currentDateSet = (TextView) findViewById(R.id.currentDateSet);
 		currentTimeSet = (TextView) findViewById(R.id.currentTimeSet);
+		map = (ImageButton) findViewById(R.id.setlocationonmap);
 
 		final Calendar c = Calendar.getInstance();
 
@@ -191,7 +195,17 @@ public class NewMeetingActivity extends ListActivity {
 		this.p_adapter = new ParticipantsAdapter(this, R.layout.meetingrow, meeting.getParticipants());
 		this.getListView().setAdapter(p_adapter);
 		infolabel.setText("New Meeting");
-
+		
+		map.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), GPSPlaceAMeeting.class);				
+				startActivityForResult(intent, REQUEST_CODE_POINTONMAP);				
+			}
+			
+		});
+		
 		/* Add participants */
 		addParticipants.setOnClickListener(new OnClickListener() {
 			@Override
@@ -213,42 +227,39 @@ public class NewMeetingActivity extends ListActivity {
 				meeting.setDuration(60); // TODO: to be implemented
 				meeting.setMonitoring(20); // TODO: to be implemented
 				meeting.settStarting(year + "-" + month + "-" + day + " " + hour + ":" + min);
-				LatLonPoint coordinates = getLatLong(getLocationInfo(nameOfPlace.getText().toString())); 
-				Log.i("Click CreateMeeting NewMeeting.java", "lat = "+coordinates.getDLatitude());
-				meeting.setLatitude(coordinates.getDLatitude());
-				meeting.setLongitude(coordinates.getDLongitude());
-				meeting.setOwner(new User(0, "", "demo@gmail.com"));
-				
-				
-				
+				// LatLonPoint coordinates = getLatLong(getLocationInfo(nameOfPlace.getText().toString()));
+				// Log.i("Click CreateMeeting NewMeeting.java", "lat = "+coordinates.getDLatitude());
+				// meeting.setLatitude(coordinates.getDLatitude());
+				// meeting.setLongitude(coordinates.getDLongitude());
+
 				// Set owner
 				SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 				User owner = new User(0, prefs.getString(Statics.USERNAME, null), prefs.getString(Statics.USEREMAIL,
 						null));
 				meeting.setOwner(owner);
-				
+
 				// Add owner to participants list
 				meeting.getParticipants().add(owner);
-
 
 				Intent intent = new Intent(view.getContext(), MeetingsListActivity.class);
 				Bundle s = new Bundle();
 				s.putByteArray("meeting", SerializerHelper.serializeObject(meeting));
 				intent.putExtras(s);
 				setResult(RESULT_OK, intent);
-				
-				/* Post the meeting*/
-                String response = ws.postMeeting(meeting);
-                int id = -1;
-                try {
-                	id = Integer.parseInt(response.split(" ")[1].split(":")[1]);
-                } catch (Exception e){
-                	Log.e(TAG, "Could not parse meeting id from:"+response);
-                	Toast.makeText(view.getContext(), "Server error: Could not parse meeting id from:"+response, Toast.LENGTH_LONG);
-                }
-                Log.d(TAG, "Parsed meeting id:"+id);
-                timeLeftToMeetingInMillisec = TimeToMeetingInLong(year, month, day, hour, min);
-                setOneTimeAlarm(timeLeftToMeetingInMillisec, id);
+
+				/* Post the meeting */
+				String response = ws.postMeeting(meeting);
+				int id = -1;
+				try {
+					id = Integer.parseInt(response.split(" ")[1].split(":")[1]);
+				} catch (Exception e) {
+					Log.e(TAG, "Could not parse meeting id from:" + response);
+					Toast.makeText(view.getContext(), "Server error: Could not parse meeting id from:" + response,
+							Toast.LENGTH_LONG);
+				}
+				Log.d(TAG, "Parsed meeting id:" + id);
+				timeLeftToMeetingInMillisec = TimeToMeetingInLong(year, month, day, hour, min);
+				setOneTimeAlarm(timeLeftToMeetingInMillisec, id);
 				finish();
 			}
 		});
@@ -274,8 +285,8 @@ public class NewMeetingActivity extends ListActivity {
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 			hour = hourOfDay;
 			min = minute;
-			/* LAYOUTPROBLEM?*/
-			currentTimeSet.setText(hour+ ":"+min);
+			/* LAYOUTPROBLEM? */
+			currentTimeSet.setText(hour + ":" + min);
 		}
 	};
 	private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
@@ -285,8 +296,8 @@ public class NewMeetingActivity extends ListActivity {
 			year = thisYear;
 			month = monthOfYear;
 			day = dayOfMonth;
-			
-			currentDateSet.setText(day+"/"+month+"-"+year);
+
+			currentDateSet.setText(day + "/" + month + "-" + year);
 
 		}
 	};
@@ -301,31 +312,30 @@ public class NewMeetingActivity extends ListActivity {
 		}
 		return null;
 	}
-	
+
 	/* Turn clock 15 min back or smth */
-	private long TimeToMeetingInLong(int newYear, int newMonth, int newDay, int newHour, int newMin){
+	private long TimeToMeetingInLong(int newYear, int newMonth, int newDay, int newHour, int newMin) {
 
-        Date d1 = new GregorianCalendar(newYear, newMonth, newDay, newHour, newMin).getTime();
-        Date today = new Date();
-        System.out.println(d1.getTime() - today.getTime());
-        System.out.println(d1.getTime() + " " + today.getTime());
-        return (d1.getTime() - today.getTime());
-    }
+		Date d1 = new GregorianCalendar(newYear, newMonth, newDay, newHour, newMin).getTime();
+		Date today = new Date();
+		System.out.println(d1.getTime() - today.getTime());
+		System.out.println(d1.getTime() + " " + today.getTime());
+		return (d1.getTime() - today.getTime());
+	}
 
-    public void setOneTimeAlarm(long timeLeftToMeetingInMillisec, int id) {
-        am = (AlarmManager) getSystemService(Context.ALARM_SERVICE); 
-        Intent meetingIntent = new Intent(this, MeetingAlarmManager.class);
-        meetingIntent.putExtra("meetingid", id);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0,
-                meetingIntent, PendingIntent.FLAG_ONE_SHOT);
+	public void setOneTimeAlarm(long timeLeftToMeetingInMillisec, int id) {
+		am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+		Intent meetingIntent = new Intent(this, MeetingAlarmManager.class);
+		meetingIntent.putExtra("meetingid", id);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, meetingIntent, PendingIntent.FLAG_ONE_SHOT);
 
-        Calendar cal = Calendar.getInstance();
-        am.set(AlarmManager.RTC_WAKEUP,
-                (System.currentTimeMillis() + timeLeftToMeetingInMillisec), pendingIntent);
-    }
+		Calendar cal = Calendar.getInstance();
+		am.set(AlarmManager.RTC_WAKEUP, (System.currentTimeMillis() + timeLeftToMeetingInMillisec), pendingIntent);
+	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d(TAG, "OnActivityResult r:"+requestCode+" res"+resultCode);
 		if (resultCode == RESULT_OK) {
 			if (requestCode == CONTACT_PICKER_RESULT) {
 				final User user = new User(-1, null, null);
@@ -379,6 +389,19 @@ public class NewMeetingActivity extends ListActivity {
 				meeting.getParticipants().add(user);
 				p_adapter.notifyDataSetChanged();
 			}
-		}
+			else if (requestCode == REQUEST_CODE_POINTONMAP) {
+				Log.d(TAG, "Returned from GPSPlaceAMetting");
+				Bundle bundle = data.getExtras();
+				if (bundle != null) {
+					int longitude = bundle.getInt("longitude");
+					int latitude = bundle.getInt("latitude");
+					Log.d(TAG, "Received coordinates from gps activity: " + latitude + "::" + longitude);
+					meeting.setLongitude(longitude);
+					meeting.setLatitude(latitude);
+				} else {
+					Log.e(TAG, "GSP activity didn't return any coordinates");
+				}
+			}
+		} 
 	}
 }
